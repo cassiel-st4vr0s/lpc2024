99409dc6548d91cef8f43387241f688e04068368
andrews <vavr.snf24@uea.edu.br>
2024-10-24 23:36:14 -0400

    added controls animation

--- atividade007/main.py
+++ atividade007/main.py
@@ -2,7 +2,7 @@ import pygame
 from button import Button
 import sys
 from random import randint
-
+from models.keys import Keys
 pygame.init()
 pygame.mixer.init()
 
@@ -16,8 +16,9 @@ Time = 0
 #game settings
 title = 'NEW GAME!'
 color = "blue"
-font = "./assets/font.ttf"
-menu_button= "./assets/menu_rect.png"
+black = (0, 0, 0)
+font = "./assets/menu/font.ttf"
+menu_button= "./assets/menu/menu_rect.png"
 window_width = 800
 window_height = 600
 
@@ -72,17 +73,16 @@ def main_menu():
 controls_not_shown = True
 def show_controls(level):
     if level == 1:
-        key_name = ['F5','F6','F7','F8']
-    if level == 2:
         key_name = ['6','7','8','9']
-    if level == 3:
+    if level == 2:
         key_name = ['y','u','i','o']
-    if level == 4:
+    if level == 3:
         key_name = ['h','j','k','l']
-    if level == 5:
-        key_name = ['n','m','comma','period']
+    if level == 4:
+        key_name = ['n','m','<','>']
 
     return key_name
+
 
 stages_passed = 0
 def stage_1():
@@ -91,11 +91,117 @@ def stage_1():
     screen.fill(color)
     screen.blit(level_1, (10, 50))
 
+    return True
 
 
 
+#load space key sprite sheet
+sprite_sheet_image = pygame.image.load("./assets/controls/SPACEALTERNATIVE.png")
+sprite_sheet = Keys(sprite_sheet_image)
+animation_list = []
+animation_steps = 3
+last_update= pygame.time.get_ticks()
+animation_cooldown = 300
+frame = 0
+
+for x in range(animation_steps):
+    animation_list.append(Keys.get_image(sprite_sheet,x,98,21,2,black))
+
+#load keys sprite sheets
+
+def load_key_sprite_sheets(level):
+    global animation_steps
+    key_1_list =[]
+    key_2_list = []
+    key_3_list = []
+    key_4_list = []
+    if level == 1:
+        key_sprite_sheet_image_1 = pygame.image.load("./assets/controls/6.png")
+        key_sprite_sheet_1 = Keys(key_sprite_sheet_image_1)
+        for x in range(animation_steps):
+            key_1_list.append(Keys.get_image(key_sprite_sheet_1,x,19,21,2,black))
+        key_sprite_sheet_image_2 = pygame.image.load("./assets/controls/7.png")
+        key_sprite_sheet_2 = Keys(key_sprite_sheet_image_2)
+        for x in range(animation_steps):
+            key_2_list.append(Keys.get_image(key_sprite_sheet_2,x,19,21,2,black))
+        key_sprite_sheet_image_3 = pygame.image.load("./assets/controls/8.png")
+        key_sprite_sheet_3 = Keys(key_sprite_sheet_image_3)
+        for x in range(animation_steps):
+            key_3_list.append(Keys.get_image(key_sprite_sheet_3,x,19,21,2,black))
+        key_sprite_sheet_image_4 = pygame.image.load("./assets/controls/9.png")
+        key_sprite_sheet_4 = Keys(key_sprite_sheet_image_4)
+        for x in range(animation_steps):
+            key_4_list.append(Keys.get_image(key_sprite_sheet_4,x,19,21,2,black))
+
+    elif level == 2:
+        key_sprite_sheet_image_1 = pygame.image.load("./assets/controls/Y.png")
+        key_sprite_sheet_1 = Keys(key_sprite_sheet_image_1)
+        for x in range(animation_steps):
+            key_1_list.append(Keys.get_image(key_sprite_sheet_1,x,19,21,2,black))
+        key_sprite_sheet_image_2 = pygame.image.load("./assets/controls/U.png")
+        key_sprite_sheet_2 = Keys(key_sprite_sheet_image_2)
+        for x in range(animation_steps):
+            key_2_list.append(Keys.get_image(key_sprite_sheet_2,x,19,21,2,black))
+        key_sprite_sheet_image_3 = pygame.image.load("./assets/controls/I.png")
+        key_sprite_sheet_3 = Keys(key_sprite_sheet_image_3)
+        for x in range(animation_steps):
+            key_3_list.append(Keys.get_image(key_sprite_sheet_3,x,19,21,2,black))
+        key_sprite_sheet_image_4 = pygame.image.load("./assets/controls/O.png")
+        key_sprite_sheet_4 = Keys(key_sprite_sheet_image_4)
+        for x in range(animation_steps):
+            key_4_list.append(Keys.get_image(key_sprite_sheet_4,x,19,21,2,black))
+    elif level == 3:
+        key_sprite_sheet_image_1 = pygame.image.load("./assets/controls/H.png")
+        key_sprite_sheet_1 = Keys(key_sprite_sheet_image_1)
+        for x in range(animation_steps):
+            key_1_list.append(Keys.get_image(key_sprite_sheet_1,x,19,21,2,black))
+        key_sprite_sheet_image_2 = pygame.image.load("./assets/controls/J.png")
+        key_sprite_sheet_2 = Keys(key_sprite_sheet_image_2)
+        for x in range(animation_steps):
+            key_2_list.append(Keys.get_image(key_sprite_sheet_2, x, 19, 21, 2, black))
+        key_sprite_sheet_image_3 = pygame.image.load("./assets/controls/K.png")
+        key_sprite_sheet_3 = Keys(key_sprite_sheet_image_3)
+        for x in range(animation_steps):
+            key_3_list.append(Keys.get_image(key_sprite_sheet_3, x, 19, 21, 2, black))
+        key_sprite_sheet_image_4 = pygame.image.load("./assets/controls/L.png")
+        key_sprite_sheet_4 = Keys(key_sprite_sheet_image_4)
+        for x in range(animation_steps):
+            key_4_list.append(Keys.get_image(key_sprite_sheet_4, x, 19, 21, 2, black))
+    else:
+        key_sprite_sheet_image_1 = pygame.image.load("./assets/controls/N.png")
+        key_sprite_sheet_1 = Keys(key_sprite_sheet_image_1)
+        for x in range(animation_steps):
+            key_1_list.append(Keys.get_image(key_sprite_sheet_1, x, 19, 21, 2, black))
+        key_sprite_sheet_image_2 = pygame.image.load("./assets/controls/M.png")
+        key_sprite_sheet_2 = Keys(key_sprite_sheet_image_2)
+        for x in range(animation_steps):
+            key_2_list.append(Keys.get_image(key_sprite_sheet_2, x, 19, 21, 2, black))
+        key_sprite_sheet_image_3 = pygame.image.load("./assets/controls/LESSTHAN.png")
+        key_sprite_sheet_3 = Keys(key_sprite_sheet_image_3)
+        for x in range(animation_steps):
+            key_3_list.append(Keys.get_image(key_sprite_sheet_3, x, 19, 21, 2, black))
+        key_sprite_sheet_image_4 = pygame.image.load("./assets/controls/GREATERTHAN.png")
+        key_sprite_sheet_4 = Keys(key_sprite_sheet_image_4)
+        for x in range(animation_steps):
+            key_4_list.append(Keys.get_image(key_sprite_sheet_4, x, 19, 21, 2, black))
+
+
+    return [key_1_list,key_2_list,key_3_list,key_4_list]
+
+
+
+
+
+#apoio
 level_controls = ['']*5
+
+
+
+
 main_menu()
+controls_screen = True
+level = randint(1,4)
+
 #game loop
 while keepRunning:
     clock = pygame.time.Clock()
@@ -104,23 +210,49 @@ while keepRunning:
         if event.type == pygame.QUIT:
             keepRunning = False
 
-    if controls_not_shown:
-        level = randint(1,5)
+    #controls screen
+
+    if controls_screen:
         level_controls = show_controls(level)
-        controls_text = get_font(10).render(f'Controls: {level_controls}', True, "white")
         screen.fill(color)
+
+        controls_text = get_font(10).render(f'Controls: {level_controls}', True, "white")
         screen.blit(controls_text, (window_width//2-150, window_height//2 - 250))
         press_space_text = get_font(10).render('Press Space to continue', True, "white")
-        screen.blit(press_space_text, (window_width//2 - 80,window_height-100))
-        controls_not_shown = False
+        screen.blit(press_space_text, (window_width//2 - 100
+                                       ,window_height-100))
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        stage_1()
-    if keys[pygame.K_ESCAPE]:
-        keepRunning = False
-        pygame.quit()
-        quit()
+
+        # update animation
+        current_time = pygame.time.get_ticks()
+        if current_time - last_update >= animation_cooldown:
+            frame += 1
+            last_update = current_time
+            if frame >= len(animation_list):
+                frame = 0
+
+        # show key frame images
+        keys_loaded = (load_key_sprite_sheets(level))
+        screen.blit(animation_list[frame], (window_width // 2 - 100, window_height - 200))
+        screen.blit(keys_loaded[0][frame],(window_width // 2 - 100,window_height - 280))
+        screen.blit(keys_loaded[1][frame],(window_width // 2 - 60,window_height - 280))
+        screen.blit(keys_loaded[2][frame],(window_width // 2 - 20,window_height - 280))
+        screen.blit(keys_loaded[3][frame],(window_width // 2 + 20,window_height - 280))
+
+
+
+
+
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            controls_screen =False
+            stage_1()
+
+        if keys[pygame.K_ESCAPE]:
+            keepRunning = False
+            pygame.quit()
+            quit()
 
 
 
